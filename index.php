9999a3f7f2c46d823ce830862e08fb43dc196429
dehamzah <dehamzah@gmail.com>
2014-12-18 15:10:59 +0700

    replacing index.php with layout-a.php

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php $base="../"; ?>
 <?php require($base."/inc/doctype.php"); ?>
-<title>Starter Files</title>
+<title>Layout A</title>
 <?php require($base."/inc/meta.php"); ?>
-<!--[if lt IE 9]><script src="../a/j/html5shiv.js"></script><![endif]-->
-<link href="../a/c/screen.css" rel="stylesheet" media="screen, projection" type="text/css" />
-<link href="/..a/c/print.css" rel="stylesheet" media="print" type="text/css" />
+<!--[if lt IE 9]><script src="/a/j/html5shiv.js"></script><![endif]-->
+<link href="../assets/css/screen.css" rel="stylesheet" media="screen, projection" type="text/css" />
+<link href="../assets/css/print.css" rel="stylesheet" media="print" type="text/css" />
+<style>
+  .main { outline: 1px dotted rgba(126, 62, 104, 1); position: relative; }
+  .main:after { position: absolute; top: -1em; left: -1em; padding: 10px; color: #fff; content: ".layout-a .main"; background: rgba(126, 62, 104, 0.7);}
+</style>
 </head>
 
-<body class="home">
+<body class="layout-a">
 
   <?php require($base."/inc/skip.php"); ?>
 
@@ -17,15 +21,11 @@
   <div class="content">
     <div class="wrap">
 
-      <section class="main" role="main">
+      <section class="main" role="main" id="skip">
 
-        <h2>Everyone Loves Lorem Ipsum!</h2>
+        <h2><a href="">Layout A</a></h2>
 
-        <p>Cupcake ipsum dolor sit amet faworki candy donut. Sweet roll brownie pie dragée. Tart gummies dragée biscuit.</p>
-
-        <p>Cake ice cream apple pie toffee. Sweet roll cake toffee sweet roll dragée jelly-o lemon drops. Tootsie roll jelly fruitcake lemon drops apple pie halvah tiramisu jelly-o.</p>
-
-        <p>Pie tart icing muffin gummi bears biscuit marzipan. Chocolate cake dragée tootsie roll. Dessert apple pie liquorice sugar plum dragée.</p>
+        <p>Layout A has one .main container and spans the full width of the page</p>     
 
       </section>
 
